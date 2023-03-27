c371028580a947de91e7dc02516d23eae0a572fe
PKSOLE <parthsolanke2002@gmail.com>
2023-03-27 19:46:25 +0530

    new

--- Open CV projects/Reverse parking assistance grid lines/cam_in(RPAS).py
+++ Open CV projects/Reverse parking assistance grid lines/cam_in(RPAS).py
@@ -1,12 +1,8 @@
-from turtle import shape
-# import the opencv library
 import cv2
-# define a video capture object
+
 vid = cv2.VideoCapture(0)
 
 while(True):
- # Capture the video frame
- # by frame
  ret, frame = vid.read()
  # Display the resulting frame
  
@@ -32,15 +28,11 @@ while(True):
  cv2.line(frame, (frame.shape[1] , frame.shape[0]) , (int(((frame.shape[1]-200)+2*frame.shape[1])/3),int((frame.shape[0]*2+200)/3)) , (0,0,255) , thickness=3)
  
  cv2.imshow('frame', frame)
-  
- # cv2.imshow('frame', frame)
- # the 'f' button is set as the
- # quitting button you may use any
- # desired button of your choice
+
+# to quit press "f"
  if cv2.waitKey(1) & 0xFF == ord('f'):
   break
 
-# After the loop release the cap object
 vid.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
