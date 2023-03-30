e437f8e738a567388e9bb1cb15e39e2a7189befd
PKSOLE <parthsolanke2002@gmail.com>
2023-03-30 18:41:38 +0530

    modified read_img(1).py

--- Basics Open CV/read_img(1).py
+++ Basics Open CV/read_img(1).py
@@ -7,6 +7,9 @@ img = cv.imread('Photos/DOG_2.jpg')
 # displaying stored image in a window
 cv.imshow('DOG' , img)
 
+# here we will try to print the image matrix to see how the image is interpreted by opencv
+print(img)
+
 # waiting for any key to be pressed to terminate the program
 cv.waitKey(0)
 
